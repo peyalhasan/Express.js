85f794b45c047817b78bfc4978def02cc86e8790
Peyal Hasan <peyalhasan143@gmail.com>
2026-03-27 18:39:42 +0600

    Serving Static Files

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 
 // Respond to GET request on the root route.
-app.get('/', (req, res) => {
-    res.send('GET request to the homepage')
-})
+// app.get('/', (req, res) => {
+//     res.send('GET request to the homepage')
+// })
 
 // Respond to POST request on the root route 
 app.post('/', (req, res) => {
@@ -72,6 +73,29 @@ app.get('/async-error', (req, res, next)=>{
 app.use((err, req, res, next) =>{
     console.error(err.stack);
     res.status(500).send('Something broke!')
+})
+
+
+// Serve static files from the 'public' directory
+
+app.use(express.static('public'));
+
+// You can also specify a virtual path prefix
+
+app.use('/static', express.static('public'))
+
+// Using absolute path (recommended)
+
+app.use('/assets', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) =>{
+    res.send(`
+        <h1>Static Files Example</h1>
+        <img src="/images/logo.png" alt="Logo" />
+        <link rel="stylesheet" href="/css/style.css" >
+        <script src="/js/script.js"></script>
+        `
+    )
 })
 
 app.listen(port, () => {
